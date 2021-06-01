b6fec29fbf9c9547fff46f0b4c0da4fcededee76
Sumit <sumitsapkota0@gmail.com>
2021-06-01 21:13:42 +0545

    sorting defination

--- main.go
+++ main.go
@@ -5,6 +5,14 @@ import (
 	"sort"
 )
 
+// Sort methods are specific to the builtin type;
+// here’s an example for strings. Note that sorting is in-place,
+// so it changes the given slice and doesn’t return a new one.
+
+// We can also use sort to check if a slice is already in sorted order.
+
+// Running our program prints the sorted string and int slices and true as the result of our AreSorted test.
+
 func main() {
 
 	strs := []string{"c", "a", "b"}
